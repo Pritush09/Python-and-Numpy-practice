b47e25934d124f2866473bd28224f7732de3074f
Pritush Kr Singh <89287734+Pritush09@users.noreply.github.com>
2022-01-10 16:22:10 +0530

    While loop
    practicing the basics of while loop

---
+++ while loop.py
@@ -0,0 +1,24 @@
+# while loop operates on the boolean value
+# it continue to perform a given task untill the given argument becomes false for the
+# given argument
+
+i = 0
+while i < 6:
+    print(i,end=" ")  # htis will print i till the time it remains less than 6 if it becomes more
+    i += 1            # it terminate the loop
+# this i +=1 will increament the i by 1 times till the loop terminate
+
+# we can also run the while loop for infinite number of times by passing the
+# argument True
+while True:
+    print("1")
+# this will print 1 which is a string in this case for infinite number of times
+
+
+
+
+
+
+
+
+
